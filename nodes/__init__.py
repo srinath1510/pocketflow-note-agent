6fe28ca6608ab0922f30d41ea737b5c6dc23c060
Srinath Srinivasan <srinath1510@gmail.com>
2025-07-30 00:22:05 -0400

    Add KnowledgeGraphNode to node package init

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -4,5 +4,6 @@ Nodes package for the AI Note Generation Pipeline
 
 from .capture_ingestion import CaptureIngestionNode
 from .content_analysis import ContentAnalysisNode
+from .knowledge_graph import KnowledgeGraphNode
 
-__all__ = ['CaptureIngestionNode', 'ContentAnalysisNode']
+__all__ = ['CaptureIngestionNode', 'ContentAnalysisNode', 'KnowledgeGraphNode']
