326fc566daf11b659dd8175b50589802983ea436
Srinath Srinivasan <srinath1510@gmail.com>
2025-08-25 19:21:46 -0400

    add missing nodes to init

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -5,5 +5,7 @@ Nodes package for the AI Note Generation Pipeline
 from .capture_ingestion import CaptureIngestionNode
 from .content_analysis import ContentAnalysisNode
 from .knowledge_graph import KnowledgeGraphNode
+from .historical_knowledge_retrieval import HistoricalKnowledgeRetrievalNode
+from .notion_note_generation import NotionNoteGenerationNode
 
-__all__ = ['CaptureIngestionNode', 'ContentAnalysisNode', 'KnowledgeGraphNode']
+__all__ = ['CaptureIngestionNode', 'ContentAnalysisNode', 'KnowledgeGraphNode', 'HistoricalKnowledgeRetrievalNode', 'NotionNoteGenerationNode']
